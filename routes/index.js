8933e9d64e5e2d23c26fc78006f993088d3d4679
Josipa314 <101945658+josipa314@users.noreply.github.com>
2022-04-26 12:27:46 +0200

    added CRUD operations, buttons and corresponding routes

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,7 @@ const Movie = require("../models/Movie.model")
 router.get('/', (req, res, next) => res.render('index'));
 
 
-
-
-
+/* MOVIES page */
 router.get("/movies", (req, res, next) => {
     Movie.find()
         .then((moviesFromDB) => {
@@ -21,6 +19,7 @@ router.get("/movies", (req, res, next) => {
 
 })
 
+/* MOVIE-DETAILS page */
 router.get("/movie/:id", (req, res, next) => {
     const id = req.params.id
 
@@ -30,5 +29,73 @@ router.get("/movie/:id", (req, res, next) => {
     })
     .catch(err => console.log("oops, there was an error" + err))
 })
+
+/* MOVIE-CREATE page path&function */
+router.get("/movies/create", (req, res, post) => {
+    res.render("new-movie")
+})
+
+router.post("/movies/create", (req, res, post) => {
+    const newMovie = {
+        title: req.body.title,
+        director: req.body.director,
+        stars: req.body.stars,
+        image: req.body.image,
+        description: req.body.description,
+        showtimes: req.body.showtimes,
+
+    }
+
+    Movie.create(newMovie)
+    .then( (movieFromDB) => {
+        res.redirect("/movies")
+    })
+    .catch(err => console.log("oops, there was an error" + err))
+});
+
+
+/* MOVIE-EDIT page path&function */
+router.get("/movies/:id/edit", (req, res, post) => {
+    const id = req.params.id
+
+    Movie.findById(id)
+    .then( (movieDetails) => {
+        res.render("movie-edit", movieDetails)
+    })
+    .catch(err => console.log("oops, there was an error" + err))
+
+})
+
+router.post("/movies/:id/edit", (req, res, post) => {
+    const id = req.params.id
+
+    const newDetails = {
+        title: req.body.title,
+        director: req.body.director,
+    }
+
+    Movie.findByIdAndUpdate(id, newDetails)
+        .then((movieFromDB) => {
+            res.redirect(`/movies`);
+        })
+        .catch(err => console.log("oops, there was an error" + err))
+});
+
+
+
+/* MOVIE-DELETE page&function */
+router.post("/movies/:id/delete", (req, res, next) => {
+    const id = req.params.id;
+    Movie.findByIdAndRemove(id)
+        .then(() => {
+            res.redirect("/movies");
+        })
+        .catch(err => {
+            console.log("oops, there was an error", err);
+            next(err);
+        });
+
+});
+
 
 module.exports = router;
